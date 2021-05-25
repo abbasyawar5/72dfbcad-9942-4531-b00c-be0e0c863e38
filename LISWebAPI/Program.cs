8e371462622342f27821e0d74de30cd7bae78ff0
abbasyawar5 <abbas.yawar5@gmail.com>
2021-05-25 20:57:22 +1000

    Commit after adding Container Orchestrator Support (Docker Compose)

--- LISWebAPI/Program.cs
+++ LISWebAPI/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LISWebAPP
+namespace LISWebAPI
 {
     public class Program
     {
---
+++ LISWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LISWebAPP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello from LISWebAPP";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Calling *LISWebAPI*, and displaying its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://LISWebAPI/WeatherForecast");   
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
